25509fc8663ce2b98e51b799682ac76c23b18e85
iman <iman@firstsource.tech>
2023-02-11 09:20:20 +0330

    fix: fixed some errors that occured in deriv-app

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: [
+        'airbnb-base',
+        'prettier',
+        'prettier/react',
+        'binary',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:jest-dom/recommended',
+    ],
+    rules: {
+    },
+};
