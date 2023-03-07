0757c4ae7d64e8230e96f4641892dd293b09f4a8
iman <iman@firstsource.tech>
2023-03-07 10:28:09 +0330

    feat: removed binary from extends list

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ module.exports = {
     'airbnb-base',
     'prettier',
     'prettier/react',
-    'binary',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:jest-dom/recommended',
